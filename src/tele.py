265fdfa993c23c43c35b3a06d8ddee6b7d4c8046
VladimirBatsura <batsura.vs@gmail.com>
2020-03-29 13:38:25 +0300

    копировали  и подстроили под телеграм

---
+++ src/tele.py
@@ -0,0 +1,97 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+from datetime import datetime
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+#
+class Phase(Enum):
+    INPUT_FIRSTNAME = auto()
+    INPUT_SURNAME = auto()
+    INPUT_BORN = auto()
+    DONE = auto()
+
+
+@dataclass()
+class PassportData:
+    firstName: str
+    surName: str
+    born: datetime
+    userId: str = "unknown"
+    phase: Phase = Phase.INPUT_FIRSTNAME
+
+    def __init__(self):
+        self.userId = self.userId
+
+
+def inputFirstName(data):
+    if data.phase is not Phase.INPUT_FIRSTNAME:
+        return
+    data.firstName = input('введи своё имя\n')
+    data.phase = Phase(data.phase.value + 1)
+
+
+def inputSurName(data):
+    if data.phase is not Phase.INPUT_SURNAME:
+        return
+    data.surName = input('введи свою фамилия\n')
+    data.phase = Phase(data.phase.value + 1)
+
+
+def printData(data):
+    print(data)
+
+
+def inputBorn(data):
+    if data.phase is not Phase.INPUT_BORN:
+        return
+    try:
+        dateStr = input('Введите дату рождения в формате дд.мм.гггг:\n')
+        data.born = datetime.strptime(dateStr, '%d.%m.%Y')
+        data.phase = Phase(data.phase.value + 1)
+    except ValueError:
+        return
+
+
+handlers = [inputFirstName, inputSurName, inputBorn]
+
+
+def handeEvent(data):
+    for handler in handlers:
+        handler(data)
+    if data.phase is Phase.DONE:
+        return False
+    else:
+        return True
+
+
+def start(update, context):
+    """Echo the user message."""
+    update.message.reply_text('меее')
+
+
+"""userData = PassportData()
+
+i = 0
+while handeEvent(userData):
+    i += 1
+
+printData(userData)
+"""
+
+def main():
+    updater = Updater("1106434818:AAH1RTWxMFDTyj9EQ3LaIUN64s-7jkeQFPU", use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
