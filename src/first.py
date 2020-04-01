622d8fc5ccd9c52f5036f7d5f8e8c9b9b062e47d
VladimirBatsura <batsura.vs@gmail.com>
2020-04-01 13:00:00 +0300

    пробный тест

--- src/first.py
+++ src/first.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from random import random
 from enum import Enum, auto
 from datetime import datetime
 import logging
@@ -13,12 +14,24 @@ logger = logging.getLogger(__name__)
 @dataclass()
 class phase:
     phase = 0
+class phase2:
+    phase2 = 0
+class help:
+    name:str
+    surName:str
+    Born: datetime
+
+def random(phase2,update):
+    d=random.randrange(1,4)
+    if d == 1:
+        update.message.reply_text('ну а теперь',)
 
 
 def hi(phase, update):
     logger.info('hi ' + str(phase.phase))
     if phase.phase == 0:
         update.message.reply_text('привет, как тебя зовут?')
+        help.name=update.message.text
         phase.phase += 1
         return True
     else:
@@ -29,6 +42,7 @@ def surname(phase, update):
     logger.info('surname'+str(phase.phase))
     if phase.phase == 1:
         update.message.reply_text('а какая фамилия у тебя?')
+        help.surName=update.message.text
         phase.phase += 1
         return True
     else:
@@ -40,14 +54,15 @@ def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
-def born(phase, update):
+def born(phase, update,phase2):
     logger.info('born'+ str(phase.phase))
     if phase.phase == 2:
         try:
             update.message.reply_text('твой год рождения в формате дд.мм.гггг')
-            god = update.message.text
+            help.Born = update.message.text
             rod = datetime.strptime(update.message.text, '%d.%m.%Y')
             phase.phase += 1
+            phase2.phase2 += 1
             return True
         except ValueError:
             return False
