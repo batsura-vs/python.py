cfe71b2b47fd59d2699b324a8afadbaa96bef5da
VladimirBatsura <batsura.vs@gmail.com>
2020-04-02 08:23:23 +0300

    удалил2

--- src/first.py
+++ src/first.py
@@ -15,9 +15,71 @@ logger = logging.getLogger(__name__)
 class Phase:
     phase = 0
     randMove = 1
-    c: int
-    a: int
-    b: int
+    c = 0
+    a = 0
+    b = 0
+
+
+class help:
+    name: str
+    surName: str
+    Born: datetime
+
+
+def startMaths(phase, update):
+    if phase.phase == 3:
+        logger.info("startMath start")
+        help.Born = datetime.strptime(update.message.text, '%d.%m.%Y')
+        if phase.randMove == 1:
+            phase.a = random.randrange(1, 200)
+            phase.b = random.randrange(1, 200)
+            phase.c = phase.a + phase.b
+            update.message.reply_text('ну а теперь ' + help.name + ' займёмся математикой \n ', phase.a, '+', phase.b,
+                                      '=')
+            chekio.check = 1
+        '''if phase.randMove == 2:
+            phase.a = random.randrange(100, 200)
+            phase.b = random.randrange(1, 100)
+            phase.c = phase.a - phase.b
+            update.message.reply_text('ну а теперь ' + help.name + ' займёмся математикой \n ', phase.a, '-', phase.b,
+                                      '=')
+            chekio.check = 2
+        if phase.randMove == 3:
+            while phase.a % phase.b != 0:
+                phase.a = random.randrange(100, 200)
+                phase.b = random.randrange(1, 100)
+                phase.c = phase.a / phase.b
+                update.message.reply_text('ну а теперь ' + help.name + ' займёмся математикой \n ', phase.a, ':',
+                                          phase.b, '=')
+                chekio.check = 3
+        if phase.randMove == 4:
+            phase.a = random.randrange(1, 10)
+            phase.b = random.randrange(1, 15)
+            phase.c = phase.a * phase.b
+            update.message.reply_text('ну а теперь ' + help.name + ' займёмся математикой \n ', phase.a, '*', phase.b,
+                                      '=')
+            chekio.check = 4'''
+
+
+def plas(phase, update):
+    if chekio.check == 1:
+        if phase.randMove == 1:
+            update.message.reply_text('молодец')
+            chekio.check = 0
+    else:
+        update.message.reply_text('неверно \n', phase.a, '+', phase.b, '=', phase.c)
+        chekio.check = 0
+
+
+def chek(phase, update):
+    if chekio.check == 1:
+        maths.d = update.message.text
+        if maths.d == phase.c:
+            update.message.reply_text('молодец')
+            chekio.check = 0
+        else:
+            update.message.reply_text('лапух!!!')
+            chekio.check = 0
 
 
 def hi(phase, update):
@@ -56,6 +118,7 @@ def born(phase, update):
         except ValueError:
             return False
 
+
 def getUserContext(update):
     key = update.message.chat.username
     userData = users.get(key)
@@ -64,11 +127,13 @@ def getUserContext(update):
         users[key] = userData
     return userData
 
+
 users = {}
 masiv = [hi, born, surname]
 
+
 def sborka(update, context):
-    phase=Phase()
+    phase = Phase()
     for handler in masiv:
         if handler(phase, update) is True:
             return
