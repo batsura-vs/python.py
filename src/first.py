9675d34016e766d821ce1470253382c19eaa824c
VladimirBatsura <batsura.vs@gmail.com>
2020-04-02 08:25:26 +0300

    удалил2

--- src/first.py
+++ src/first.py
@@ -133,7 +133,7 @@ masiv = [hi, born, surname]
 
 
 def sborka(update, context):
-    phase = getUserContext()
+    phase = getUserContext(update)
     for handler in masiv:
         if handler(phase, update) is True:
             return
