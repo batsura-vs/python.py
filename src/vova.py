66550d3a5f95ed8ac9a8df2c3ec720cb0da97a18
VladimirBatsura <batsura.vs@gmail.com>
2020-04-03 20:21:04 +0300

    починил ошибку

--- src/vova.py
+++ src/vova.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 @dataclass
 class sups:
     d:str
+    c:str
 def sup():
     d=input('какой суп вам приготовить?\n           меню          \n1.борщь \n2.куриный бульон \nчто вы выбрали?\n')
     #повтори как срочкой выше только макорошки и пюрешка
