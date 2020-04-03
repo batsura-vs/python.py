8981b13d183125941e1e544683cc745efb854a84
VladimirBatsura <batsura.vs@gmail.com>
2020-04-03 20:17:15 +0300

    добавил дата класс ,ввод с новой строки

--- src/vova.py
+++ src/vova.py
@@ -1,3 +1,6 @@
-def sup():
-    d=input('какой суп вам приготовить?\n           меню          \n1.борщь \n2.куриный бульон \nчто вы выбрали?')
+from dataclasses import dataclass
+@dataclass
+class sups:
 
+def sup():
+    d=input('какой суп вам приготовить?\n           меню          \n1.борщь \n2.куриный бульон \nчто вы выбрали\n?')
